2171fc93545e60df7a122581af72b0e78bae4031
webfc <webfcosta@gmail.com>
2019-03-22 13:22:07 -0300

    Change site dir

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Acessar &lsaquo; Web F.C. &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://webfc.github.io/webfabiocostasite/wp-includes/css/dashicons.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://webfc.github.io/webfabiocostasite/wp-includes/css/buttons.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://webfc.github.io/webfabiocostasite/wp-admin/css/forms.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://webfc.github.io/webfabiocostasite/wp-admin/css/l10n.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://webfc.github.io/webfabiocostasite/wp-admin/css/login.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://https://webfc.github.io/digitalfc/wp-includes/css/dashicons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://https://webfc.github.io/digitalfc/wp-includes/css/buttons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://https://webfc.github.io/digitalfc/wp-admin/css/forms.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://https://webfc.github.io/digitalfc/wp-admin/css/l10n.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://https://webfc.github.io/digitalfc/wp-admin/css/login.min.css?ver=5.1.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://br.wordpress.org/" title="Powered by WordPress">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="https://webfc.github.io/webfabiocostasite/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://https://webfc.github.io/digitalfc/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Nome de usuário ou endereço de e-mail<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -34,13 +34,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Lembrar-me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acessar" />
-				<input type="hidden" name="redirect_to" value="https://webfc.github.io/webfabiocostasite/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://https://webfc.github.io/digitalfc/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="https://webfc.github.io/webfabiocostasite/wp-login.php?action=lostpassword">Perdeu a senha?</a>
+					<a href="https://https://webfc.github.io/digitalfc/wp-login.php?action=lostpassword">Perdeu a senha?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="https://webfc.github.io/webfabiocostasite/">
+			<p id="backtoblog"><a href="https://https://webfc.github.io/digitalfc/">
 		&larr; Voltar para Web F.C.	</a></p>
 			
 	</div>
